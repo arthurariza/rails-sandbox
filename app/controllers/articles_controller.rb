570d8c09b8572d820a72643ca77ec3fdb2af4473
arthurariza <tucaariza@gmail.com>
2023-04-27 14:46:40 -0300

    Simple blog

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,52 @@
+class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @comments = @article.comments
+    @comment = Comment.new
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @article.destroy
+
+    redirect_to articles_url, status: :see_other
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :body, :status)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root 'pages#index'
+  root 'articles#index'
 
   get '/pages', to: 'pages#index'
+
+  resources :articles do
+    resources :comments
+  end
 end
